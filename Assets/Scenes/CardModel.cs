58bad43a44682810ccd7d636d54f2d43ba54ec0d
yuki0035vn@gmail.com <yuki0035vn@gmail.com>
2019-08-15 23:13:59 +0700

    AA

---
+++ Assets/Scenes/CardModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Card Game", menuName = "Card/Card 13")]
+public class CardModel : ScriptableObject
+{
+    public int number;
+    public Color_Card color;
+    public Ki_Hieu kihieu;
+    public Sprite image;
+}
+public enum Color_Card
+{
+    Red,Black
+}
+public enum Ki_Hieu
+{
+    Nothing,J,Q,K,A
+}
